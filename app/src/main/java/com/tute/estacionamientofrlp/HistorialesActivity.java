fa073d69259e46e88b2ace18c35c37f561505e4f
tutee <tute.guazzaroni@gmail.com>
2016-10-18 14:10:10 -0300

    18/10
    ListView Compras funcionando sin optimizar

--- app/src/main/java/com/tute/estacionamientofrlp/HistorialesActivity.java
+++ app/src/main/java/com/tute/estacionamientofrlp/HistorialesActivity.java
@@ -1,5 +1,6 @@
 package com.tute.estacionamientofrlp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,11 +9,32 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Tute on 26/9/2016.
@@ -24,6 +46,10 @@ public class HistorialesActivity extends AppCompatActivity implements Navigation
     private Session session;
     private Spinner spi;
     private ListView li;
+    ArrayList compras = new ArrayList<String>();
+    List<String> comppatecod = new ArrayList<String>();
+    List<String> compfechas = new ArrayList<String>();
+
 
 
     @Override
@@ -33,6 +59,7 @@ public class HistorialesActivity extends AppCompatActivity implements Navigation
         spi = (Spinner)findViewById(R.id.spinner);
         li = (ListView) findViewById(R.id.list);
 
+        cargarlista(VarGlobales.cUid,VarGlobales.email);
 
         session = new Session(HistorialesActivity.this);
 
@@ -54,6 +81,128 @@ public class HistorialesActivity extends AppCompatActivity implements Navigation
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spi.setAdapter(adapter);
 
+        /*ListAdapter adapterlist = new ListAdapter(this, comppatecod, compfechas);
+        li.setAdapter(adapterlist);*/
+
+    }
+
+
+    private void cargarlista (final String uId, final String uEmail) {
+        // Tag used to cancel the request
+        String tag_string_req = "req_cargarlista";
+
+
+        StringRequest strReq = new StringRequest(Request.Method.POST,
+                AppURLs.URL, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.e("ERROR Saldo", response);
+                Log.e("id", uId);
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+
+                    if (!error) {
+
+                        JSONArray arrayC = jObj.getJSONArray("compras");
+                        for (int h = 0; h < arrayC.length(); h++ ){
+
+
+                            JSONObject jObjP = arrayC.getJSONObject(h);
+                            compras.add(jObjP);
+                            comppatecod.add(jObjP.getString("comp_pate_cod"));
+                            compfechas.add(jObjP.getString("comp_fecha"));
+                        }
+
+                        Log.e("COMPRAS", String.valueOf(compras));
+                        Log.e("PATES", String.valueOf(comppatecod));
+                        Log.e("FECHAS", String.valueOf(compfechas));
+
+                        ListAdapter adapterlist = new ListAdapter(HistorialesActivity.this, comppatecod, compfechas);
+                        li.setAdapter(adapterlist);
+
+                    } else {
+
+                        String errorMsg = jObj.getString("error_msg");
+                        Toast.makeText(getApplicationContext(),
+                                errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting params to register url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("tag", "listcomprasusu");
+                params.put("pers_id", uId);
+                params.put("pers_email", uEmail);
+
+                return params;
+            }
+
+        };
+
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+    }
+
+
+    class ListAdapter extends BaseAdapter {
+
+        Context context;
+        List<String> pates = comppatecod;
+        List<String> cf = compfechas;
+        LayoutInflater inflater;
+
+        public ListAdapter(Context context, List<String> pates, List<String> cf){
+            this.context = context;
+            this.pates = pates;
+            this.cf = cf;
+        }
+
+
+        @Override
+        public int getCount() {
+            return pates.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+
+
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View itemView = inflater.inflate(R.layout.single_row,parent,false);
+            TextView pate = (TextView) itemView.findViewById(R.id.pate);
+            TextView dia = (TextView) itemView.findViewById(R.id.dia);
+
+            pate.setText("Patente: " + pates.get(position));
+            dia.setText(cf.get(position));
+
+            return itemView;
+        }
     }
 
     @Override
