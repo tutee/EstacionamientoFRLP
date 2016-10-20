8d4d1ef0881e9080d8cd339338e343ac5f8755f4
tutee <tute.guazzaroni@gmail.com>
2016-10-20 12:49:42 -0300

    20/10  LISTA GUARDIA FUNCIONANDO

--- app/src/main/java/com/tute/estacionamientofrlp/VerComprasActivity.java
+++ app/src/main/java/com/tute/estacionamientofrlp/VerComprasActivity.java
@@ -16,6 +16,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListAdapter;
@@ -50,6 +51,8 @@ public class VerComprasActivity extends AppCompatActivity implements NavigationV
     private ListView li;
     private ProgressDialog Dialog;
     List<String> patecomp = new ArrayList<String>();
+    List<String> accetipo = new ArrayList<String>();
+    List<String> compid = new ArrayList<String>();
 
 
     @Override
@@ -85,6 +88,141 @@ public class VerComprasActivity extends AppCompatActivity implements NavigationV
 
         cargarlista(VarGlobales.cUid);
 
+        li.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                if (accetipo.get(position).equals("Comprado")){
+                    ingreso (VarGlobales.cUid, compid.get(position));
+                } else if (accetipo.get(position).equals("Ingreso")) {
+                    egreso(VarGlobales.cUid, compid.get(position));
+                } else if (accetipo.get(position).equals("Egreso")) {
+                    ingreso (VarGlobales.cUid, compid.get(position));
+                }
+                //Toast.makeText(getApplicationContext(), "presiono " + compid.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void ingreso (final String uId, final String cId) {
+        // Tag used to cancel the request
+        String tag_string_req = "req_ingreso";
+
+        showDialog(); // CAMBIAR
+
+        StringRequest strReq = new StringRequest(Request.Method.POST,
+                AppURLs.URL, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.e("RESPUESTA", response);
+                hideDialog();
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+
+                    if (!error) {
+
+                        Toast.makeText(getApplicationContext(),
+                                "Cambio realizado", Toast.LENGTH_LONG).show();
+
+                        cargarlista(VarGlobales.cUid);
+
+                    } else {
+
+                        String errorMsg = jObj.getString("error_msg");
+                        Toast.makeText(getApplicationContext(),
+                                errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting params to register url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("tag", "ingreso");
+                params.put("guardia_id", uId);
+                params.put("comp_id", cId);
+
+                return params;
+            }
+
+        };
+
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+    }
+
+    private void egreso (final String uId, final String cId) {
+        // Tag used to cancel the request
+        String tag_string_req = "req_egreso";
+
+        showDialog(); // CAMBIAR
+
+        StringRequest strReq = new StringRequest(Request.Method.POST,
+                AppURLs.URL, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.e("RESPUESTA", response);
+                hideDialog();
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+
+                    if (!error) {
+
+                        Toast.makeText(getApplicationContext(),
+                                "Cambio realizado", Toast.LENGTH_LONG).show();
+
+                        cargarlista(VarGlobales.cUid);
+
+                    } else {
+
+                        String errorMsg = jObj.getString("error_msg");
+                        Toast.makeText(getApplicationContext(),
+                                errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting params to register url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("tag", "egreso");
+                params.put("guardia_id", uId);
+                params.put("comp_id", cId);
+
+                return params;
+            }
+
+        };
+
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
     }
 
     private void cargarlista (final String uId) {
@@ -94,6 +232,8 @@ public class VerComprasActivity extends AppCompatActivity implements NavigationV
         showDialog(); // CAMBIAR
 
         patecomp = new ArrayList<String>();
+        accetipo = new ArrayList<String>();
+        compid = new ArrayList<String>();
 
         StringRequest strReq = new StringRequest(Request.Method.POST,
                 AppURLs.URL, new Response.Listener<String>() {
@@ -115,13 +255,19 @@ public class VerComprasActivity extends AppCompatActivity implements NavigationV
 
                             JSONObject jObjP = arrayC.getJSONObject(h);
                             patecomp.add(jObjP.getString("comp_pate_cod"));
+                            if(jObjP.getString("acce_tipo").equals("null")){
+                                accetipo.add("Comprado");
+                            } else {
+                                accetipo.add(jObjP.getString("acce_tipo"));
+                            }
+                            compid.add(jObjP.getString("comp_id"));
 
                         }
 
                         Log.e("COMPRAS", String.valueOf(patecomp));
 
 
-                        ListAdapterCompras listAdapterComp = new ListAdapterCompras(VerComprasActivity.this, patecomp);
+                        ListAdapterCompras listAdapterComp = new ListAdapterCompras(VerComprasActivity.this, compid, patecomp, accetipo);
                         li.setAdapter(listAdapterComp);
                         hideDialog();
 
@@ -163,11 +309,15 @@ public class VerComprasActivity extends AppCompatActivity implements NavigationV
 
         Context context;
         List<String> pates = patecomp;
+        List<String> actp = accetipo;
+        List<String> cpid = compid;
         LayoutInflater inflater;
 
-        public ListAdapterCompras(Context context, List<String> pates){
+        public ListAdapterCompras(Context context,List<String> cpid, List<String> pates,  List<String> actp){
             this.context = context;
+            this.cpid = cpid;
             this.pates = pates;
+            this.actp = actp;
         }
 
 
@@ -197,6 +347,9 @@ public class VerComprasActivity extends AppCompatActivity implements NavigationV
 
             TextView patentes = (TextView) itemView.findViewById(R.id.pate);
             patentes.setText("Patente: " + pates.get(position));
+
+            TextView accesos = (TextView) itemView.findViewById(R.id.estado);
+            accesos.setText("Estado: " + actp.get(position));
 
             return itemView;
         }
